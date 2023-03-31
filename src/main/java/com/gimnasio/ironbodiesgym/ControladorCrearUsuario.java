0c7172120f7e211ef4741d61d002e5a1a47a326b
CarlitosHV <carloshvsa21@hotmail.com>
2023-03-31 14:35:40 -0600

    Cambios menores en interfaces existentes

--- src/main/java/com/gimnasio/ironbodiesgym/ControladorCrearUsuario.java
+++ src/main/java/com/gimnasio/ironbodiesgym/ControladorCrearUsuario.java
@@ -1,14 +1,29 @@
 package com.gimnasio.ironbodiesgym;
 
+import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 public class ControladorCrearUsuario {
 
     public Button Boton;
+    @FXML
+    private AnchorPane rootPane;
 
 
-    public void Clic(){
-        System.out.println("Un click");
+    public void Regresar(){
+        FadeTransition fadeTransition = new FadeTransition();
+        fadeTransition.setDuration(Duration.millis(500));
+        fadeTransition.setNode(rootPane);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.setOnFinished(actionEvent -> ViewSwitcher.switchTo(View.LOGIN));
+        fadeTransition.play();
     }
+
 
 }
