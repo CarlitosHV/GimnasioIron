ac9d52826b21eb6258d57f491c12e66043304256
unknown <lrodriguezg004@alumno.uaemex.mx>
2023-04-13 16:23:09 -0600

    Añadí interfaces2

--- src/main/java/com/gimnasio/ironbodiesgym/View.java
+++ src/main/java/com/gimnasio/ironbodiesgym/View.java
@@ -3,12 +3,8 @@ package com.gimnasio.ironbodiesgym;
 public enum View {
     LOGIN("VistaLogin.fxml"),
     CREAR_USUARIO("VistaCrearUsuario.fxml"),
-<<<<<<< HEAD
-
-    RENOVAR_SUSCRIPCIONES("VistaRenovarSuscripciones.fxml");
-=======
+    RENOVAR_SUSCRIPCIONES("VistaRenovarSuscripciones.fxml"),
     RECUPERAR_CONTRASENA("VistaRecuperarContrasena.fxml");
->>>>>>> 1fd2400260384eb98c846309cccbf4f240135bec
 
     private String fileName;
 
