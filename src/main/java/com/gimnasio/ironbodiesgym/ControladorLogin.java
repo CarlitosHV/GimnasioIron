036279bbb1bf1b1ea5bb22a7ae0aab2edb0d2a98
CarlitosHV <carloshvsa21@hotmail.com>
2023-04-13 18:13:30 -0600

    pruebas de cifrado correctas

--- src/main/java/com/gimnasio/ironbodiesgym/ControladorLogin.java
+++ src/main/java/com/gimnasio/ironbodiesgym/ControladorLogin.java
@@ -2,15 +2,24 @@ package com.gimnasio.ironbodiesgym;
 
 import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
+import java.security.MessageDigest;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 
 public class ControladorLogin
 {
     @FXML
     private AnchorPane rootPane;
+    @FXML
+    private Button Boton_ingresar;
+    @FXML
+    private PasswordField Campo_contra;
   public void Crear_usuario(){
       FadeTransition fadeTransition = new FadeTransition();
       fadeTransition.setDuration(Duration.millis(500));
@@ -30,5 +39,47 @@ public class ControladorLogin
       fadeTransition.setOnFinished(actionEvent -> ViewSwitcher.switchTo(View.RECUPERAR_CONTRASENA));
       fadeTransition.play();
   }
+
+
+  @FXML
+  void Contra(ActionEvent event) throws Exception {
+      String contrasenia = Campo_contra.getText();
+      byte[] contra = cifra(contrasenia);
+      String descifra = descifra(contra);
+      System.out.println(contra);
+      System.out.println("-----------------");
+      System.out.println(descifra);
+
+  }
+
+    public byte[] cifra(String sinCifrar) throws Exception {
+        final byte[] bytes = sinCifrar.getBytes("UTF-8");
+        final Cipher aes = obtieneCipher(true);
+        final byte[] cifrado = aes.doFinal(bytes);
+        return cifrado;
+    }
+
+    public String descifra(byte[] cifrado) throws Exception {
+        final Cipher aes = obtieneCipher(false);
+        final byte[] bytes = aes.doFinal(cifrado);
+        final String sinCifrar = new String(bytes, "UTF-8");
+        return sinCifrar;
+    }
+
+    private Cipher obtieneCipher(boolean paraCifrar) throws Exception {
+        final String frase = "NuestraLLave";
+        final MessageDigest digest = MessageDigest.getInstance("SHA");
+        digest.update(frase.getBytes("UTF-8"));
+        final SecretKeySpec key = new SecretKeySpec(digest.digest(), 0, 16, "AES");
+
+        final Cipher aes = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        if (paraCifrar) {
+            aes.init(Cipher.ENCRYPT_MODE, key);
+        } else {
+            aes.init(Cipher.DECRYPT_MODE, key);
+        }
+
+        return aes;
+    }
 
 }
