7fcc7776cd3154c91dd585c3e4a3d927a31581e5
CarlitosHV <carloshvsa21@hotmail.com>
2023-04-22 18:02:46 -0600

    Ventana de recuperar contraseña terminada(FrontEnd)

--- src/main/java/com/gimnasio/ironbodiesgym/ControladorRecuperarContrasena.java
+++ src/main/java/com/gimnasio/ironbodiesgym/ControladorRecuperarContrasena.java
@@ -1,14 +1,46 @@
 package com.gimnasio.ironbodiesgym;
 
 import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 
-public class ControladorRecuperarContrasena {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControladorRecuperarContrasena implements Initializable {
 
     @FXML
+    private TextField Campo_correo;
+    @FXML
+    private PasswordField Campo_contrasenia, Campo_repetir_contrasena;
+    @FXML
+    private Button Boton_validar;
+    @FXML
     public AnchorPane PanelPrin;
+
+    @FXML
+    void ValidarCorreo(){
+        /*
+        Aquí va a ir la validación del correo y traerlo para mostrarlo en pantalla
+         */
+        switch (Boton_validar.getText()){
+            case "Validar":
+                //Traemos la info de la bd
+                TrasladarItems();
+                break;
+            case "Actualizar":
+                //Mandamos a actualizar la nueva contraseña
+                break;
+        }
+
+    }
 
     @FXML
     void RegresarLogin(){
@@ -20,4 +52,45 @@ public class ControladorRecuperarContrasena {
         fadeTransition.setOnFinished(actionEvent -> ViewSwitcher.switchTo(View.LOGIN, IndexApp.Tema));
         fadeTransition.play();
     }
+
+    @FXML
+    void TrasladarItems(){
+        /*
+            Si encuentra el correo en BD, nos muestra los demás campos y los reacomoda y
+            también desactiva el campo correo
+         */
+        animar(Campo_correo, -15.0);
+        animar(Boton_validar, 120.0);
+
+    }
+
+    private void animar(Node elemento, double Y){
+        TranslateTransition transition = new TranslateTransition();
+        transition.setNode(elemento);
+        transition.setByY(Y);
+        transition.setDuration(Duration.seconds(2.0));
+        transition.play();
+        transition.setOnFinished(event -> {
+            Campo_contrasenia.setVisible(true);
+            Campo_repetir_contrasena.setVisible(true);
+            Boton_validar.setText("Actualizar");
+            Campo_correo.setEditable(false);
+        });
+    }
+
+    void OcultarItems(){
+        //Ocultamos los campos que no se muestran en un principio y trasladamos los que se ven un poco al centro
+        Campo_contrasenia.setVisible(false);
+        Campo_repetir_contrasena.setVisible(false);
+        Campo_correo.setLayoutX(123.0);
+        Campo_correo.setLayoutY(152.0);
+        Boton_validar.setLayoutX(225.0);
+        Boton_validar.setLayoutY(205.0);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        OcultarItems();
+    }
+
 }
